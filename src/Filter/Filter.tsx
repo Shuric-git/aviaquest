36c9874e4d9d06a902572d612bbe5c10c8bccacf
Shuric-git <ulme@rambler.ru>
2022-07-14 16:41:12 +0300

    add checkboxes logic

--- src/Filter/Filter.tsx
+++ src/Filter/Filter.tsx
@@ -1,44 +1,41 @@
-import { useRef } from 'react';
+// import { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { filterItems } from '../utils/filterItems';
 
 import './Filter.css';
 
 export const Filter = () => {
-  const refCheckbox = useRef(null);
-  console.log(refCheckbox);
+  const dispatch = useDispatch();
+  const check = useSelector((state: []) => state);
+  const toggleCheck = (index: number) => {
+    dispatch({ type: 'clickCheck', payload: index });
+  };
+
+  console.log(check);
   return (
     <>
       <div className="filter">
         <span className="filter__title">КОЛИЧЕСТВО ПЕРЕСАДОК</span>
-        <div className="filter__checkboxWrapper">
-          <input id="transfer-all" type="checkbox" checked={true} ref={refCheckbox} />
-          <label htmlFor="transfer-all">
-            <span>Все</span>
-          </label>
-        </div>
-        <div className="filter__checkboxWrapper">
-          <input id="transfer-without" type="checkbox" />
-          <label htmlFor="transfer-without">
-            <span>Без пересадок</span>
-          </label>
-        </div>
-        <div className="filter__checkboxWrapper">
-          <input id="transfer-1" type="checkbox" />
-          <label htmlFor="transfer-1">
-            <span>1 пересадка</span>
-          </label>
-        </div>
-        <div className="filter__checkboxWrapper">
-          <input id="transfer-2" type="checkbox" />
-          <label htmlFor="transfer-2">
-            <span>2 пересадки</span>
-          </label>
-        </div>
-        <div className="filter__checkboxWrapper">
-          <input id="transfer-3" type="checkbox" />
-          <label htmlFor="transfer-3">
-            <span>3 пересадки</span>
-          </label>
-        </div>
+        <ul className="filter__list">
+          {filterItems.map(({ name }, index) => {
+            return (
+              <li key={index}>
+                <div className="filter__checkboxWrapper">
+                  <input
+                    id={`custom-checkbox-${index}`}
+                    type="checkbox"
+                    checked={check[index]}
+                    onChange={() => toggleCheck(index)}
+                  />
+                  <label htmlFor={`custom-checkbox-${index}`}>
+                    <span>{name}</span>
+                  </label>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { createStore } from 'redux';
 import './index.css';
+import { Provider } from 'react-redux';
+
+import { filterItems } from './utils/filterItems';
 import App from './App/App';
 
+const defaultState = new Array(filterItems.length).fill(false);
+
+const reducer = (state: boolean[] = defaultState, action: { type: string; payload: number }) => {
+  switch (action.type) {
+    case 'clickCheck':
+      if (action.payload === 0 && state[0]) {
+        return defaultState;
+      }
+      if (action.payload === 0 && !state[0]) {
+        return defaultState.map((item) => !item);
+      }
+      return state.map((item, index) => {
+        return index === action.payload ? !item : item;
+      });
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
